5eb711233af39971e9b11fb4174af3e39c0d47db
Saharsh Hariharan <saharsh2010@hotmail.com>
2017-09-29 00:24:44 -0400

    Add files via upload

---
+++ ConsoleOutput.java
@@ -0,0 +1,14 @@
+import hsa.Console;
+/** The "ConsoleOutput" class.
+ * Purpose: Displays a Welcome Message
+ * @Saharsh Hariharan
+ * @Sept7 2017
+ */
+public class ConsoleOutput
+{
+ public static void main (String [] args)
+ {
+ Console myConsole = new Console ("First Java Program");
+ myConsole.println ("Welcome to Java");
+ } // main method
+} // ConsoleOutput class
